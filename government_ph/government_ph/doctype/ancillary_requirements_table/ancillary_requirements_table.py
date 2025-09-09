69310ad6803734eb072cd7a02ea21a930ce2f88b
David Webb Espiritu <davidwebbespiritu@gmail.com>
2025-09-09 20:08:51 +0800

    commit

--- government_ph/government_ph/doctype/ancillary_requirements_table/ancillary_requirements_table.py
+++ government_ph/government_ph/doctype/ancillary_requirements_table/ancillary_requirements_table.py
@@ -6,4 +6,35 @@ from frappe.model.document import Document
 
 
 class AncillaryRequirementsTable(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		approved_by: DF.Link | None
+		approved_date: DF.Datetime | None
+		assigned_departmentoffice: DF.Data | None
+		assigned_role: DF.Link | None
+		clearance_no: DF.Data | None
+		create_compliance_inspection: DF.Link | None
+		document_title: DF.Link | None
+		file: DF.Attach | None
+		file_attached_by: DF.Link | None
+		file_attachment_date: DF.Datetime | None
+		notesremarks: DF.SmallText | None
+		parent: DF.Data
+		parentfield: DF.Data
+		parenttype: DF.Data
+		rejected_by: DF.Link | None
+		rejected_date: DF.Datetime | None
+		resubmitted_by: DF.Link | None
+		resubmitted_date: DF.Datetime | None
+		resubmitted_file: DF.Attach | None
+		status: DF.Literal["Awaiting Submission", "For Inspection", "Pending Review", "Approved", "Rejected", "For Resubmission"]
+		valid_from: DF.Date | None
+		valid_until: DF.Date | None
+	# end: auto-generated types
 	pass
--- government_ph/permits_and_licensing/doctype/compliance_inspection/compliance_inspection.py
+++ government_ph/permits_and_licensing/doctype/compliance_inspection/compliance_inspection.py
@@ -6,4 +6,22 @@ from frappe.model.document import Document
 
 
 class ComplianceInspection(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		actual_visit: DF.Date | None
+		amended_from: DF.Link | None
+		business_permit_application: DF.Link | None
+		departmentoffice: DF.Link | None
+		inspected_by: DF.Link | None
+		long_text_zpbp: DF.LongText | None
+		rating: DF.Literal["Compliant", "Non-Compliant"]
+		scheduled_visit: DF.Date | None
+		verified_by: DF.Link | None
+	# end: auto-generated types
 	pass
